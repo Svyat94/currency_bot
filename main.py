da4ffe2b95251b170339315465c9779f59461964
svyat94 <vinogradov.s94@yandex.ru>
2023-10-31 10:58:48 +0300

    добавлен обработчик сообщений

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from datetime import datetime
 import telebot
 from pycbrf import ExchangeRates
+from datetime import datetime, timedelta
 
 
 load_dotenv()
@@ -30,6 +31,20 @@ def message(message):
     if message_norm in ['usd', 'eur', 'cny', 'kzt']:
         rates = ExchangeRates(datetime.now())
         bot.send_message(chat_id=message.chat.id, text=f'<b>Курс {message_norm.upper()} = {float(rates[message_norm.upper()].rate)} рублей</b>', parse_mode='html')
+    
+    else:
+        reply_text = "Пока я не умею распознавать такие команды, но мой создатель в скором времени все добавит. А пока вы можете посмотреть текущий курс интересующей вас валюты."
+        markup = get_currency_keyboard()
+        bot.send_message(chat_id=message.chat.id, text=reply_text, reply_markup=markup, parse_mode='html')
+
+def get_currency_keyboard():
+    markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
+    itembtn1 = telebot.types.KeyboardButton('USD')
+    itembtn2 = telebot.types.KeyboardButton('EUR')
+    itembtn3 = telebot.types.KeyboardButton('CNY')
+    itembtn4 = telebot.types.KeyboardButton('KZT')
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
+    return markup
 
 
 bot.polling(non_stop=True)
